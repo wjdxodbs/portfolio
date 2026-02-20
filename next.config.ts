80e8358d3061e156620ea42910427f9659d016d7
wjdxodbs <wjdxodbs52@naver.com>
2026-02-20 22:56:38 +0900

    chore: 기술 스택 아이콘 유틸 추가 및 이미지 도메인 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.jsdelivr.net",
+      },
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
